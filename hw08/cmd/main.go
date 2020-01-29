b047e1c15dd981e0d19566c1e7217c4477287637
Sergey Gulinov <sgulinov@stream.ru>
2020-01-29 18:49:19 +0300

    Home work 08 remove test code from main

--- hw08/cmd/main.go
+++ hw08/cmd/main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
-	"time"
 
 	"github.com/dark705/otus/hw08/internal/Calendar/Calendar"
-	"github.com/dark705/otus/hw08/internal/Calendar/Event"
 	"github.com/dark705/otus/hw08/internal/Config"
 	"github.com/dark705/otus/hw08/internal/Logger"
 	"github.com/dark705/otus/hw08/internal/Storage"
@@ -23,22 +21,5 @@ func main() {
 
 	inMemory := Storage.InMemory{}
 	inMemory.Init()
-	calendar := Calendar.Calendar{Config: config, Storage: &inMemory, Logger: logger}
-	ev, _ := calendar.GetAllEvents()
-	fmt.Println(ev)
-
-	event1, _ := Event.GetEvent("2006-01-02T15:00:00Z", "2006-01-02T16:00:00Z", "Event 1", "Some Desc1")
-	event2, _ := Event.GetEvent("2006-01-02T16:00:00Z", "2006-01-02T17:00:00Z", "Event 2", "Some Desc2")
-	event3, _ := Event.GetEvent("2006-01-02T17:00:00Z", "2006-01-02T18:00:00Z", "Event 3", "Some Desc3")
-
-	calendar.AddEvent(event1)
-	calendar.AddEvent(event2)
-	calendar.AddEvent(event3)
-	event2s, _ := calendar.GetEvent(1)
-	event2s.EndTime, _ = time.Parse(time.RFC3339, "2006-01-02T16:30:00Z")
-	fmt.Println(inMemory)
-	calendar.EditEvent(event2s)
-	ev2, _ := calendar.GetAllEvents()
-	fmt.Println(ev2)
-
+	_ = Calendar.Calendar{Config: config, Storage: &inMemory, Logger: logger}
 }
