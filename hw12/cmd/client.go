88087dc9c794968c206257c05dcd8a6f91fa6de3
dark705 <dark705r@mail.ru>
2020-03-01 00:29:46 +0300

    Home work 12 #test sql

--- hw12/cmd/client.go
+++ hw12/cmd/client.go
@@ -7,9 +7,9 @@ import (
 	"os"
 	"time"
 
+	"github.com/dark705/otus/hw12/internal/calendar/event"
 	_ "github.com/jackc/pgx/stdlib"
 
-	"github.com/dark705/otus/hw12/internal/calendar/event"
 	"github.com/dark705/otus/hw12/internal/config"
 	"github.com/dark705/otus/hw12/internal/logger"
 	"github.com/jmoiron/sqlx"
@@ -39,57 +39,124 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-
 	ctxExec, _ := context.WithTimeout(context.Background(), time.Second*time.Duration(conf.PgTimeoutExecute))
-	rows, err := db.QueryxContext(ctxExec, "select * from events")
+	/*
+		Add(e event.Event) error
+		Get(id int) (event.Event, error)
+		Del(id int) error
+		GetAll() ([]event.Event, error)
+		Edit(event.Event) error
+		IntervalIsBusy(event.Event, bool) (bool, error)
+	*/
+
+	ev, err := event.CreateEvent("2006-01-02T15:00:00Z", "2006-01-02T16:00:00Z", "Event 1", "Some Desc1")
 	if err != nil {
-		log.Errorln("Fail on db quiery:")
-	} else {
-		for rows.Next() {
-			var e event.Event
-			if err := rows.StructScan(&e); err != nil {
-				//TODO
-			}
-			fmt.Println(e)
-		}
+		log.Errorln("Cant parse", err)
 	}
-	fmt.Println("************")
-	events := []event.Event{}
-	err = db.Select(&events, "select * from events")
-	fmt.Println(events, err)
+
+	//Add
+	sql := "INSERT INTO events (start_time, end_time, title, description) VALUES (:start_time, :end_time, :title, :description);" // :start_time from `db:"start_time"` and so on
+	_, err = db.NamedExecContext(ctxExec, sql, ev)
+	if err != nil {
+		log.Errorln("Fail on add event to PG", err)
+	}
+
+	//Get
+	sql = "SELECT * FROM events WHERE id = :id;" // :id from `db:"id"`
+	rows, err := db.NamedQueryContext(ctxExec, sql, struct {
+		Id int `db:"id"`
+	}{Id: 58})
+	if err != nil {
+		log.Errorln("Fail on get event from PG", err)
+	}
+	rows.Next()
+	var e event.Event
+	if err := rows.StructScan(&e); err != nil {
+		log.Errorln("Fail on get event from PG", err)
+	}
+	fmt.Println(e)
+
+	//Del
+	sql = "DELETE FROM events WHERE id = :id;"
+	res, err1 := db.NamedExecContext(ctxExec, sql, struct {
+		Id int `db:"id"`
+	}{Id: 65})
+
+	if err1 != nil {
+		log.Errorln("Fail on del event in PG", err)
+	}
+	aff, err := res.RowsAffected()
+	fmt.Println(aff, err)
+	if aff != 1 || err != nil {
+		log.Errorln("Fail on del event in PG")
+	}
+
+	//GetAll()
+	sql = "SELECT * FROM events;" // :id from `db:"id"`
+	rows, err = db.QueryxContext(ctxExec, sql)
+	if err != nil {
+		log.Errorln("Fail on get event from PG", err)
+	}
+	var events []event.Event
+	for rows.Next() {
+		var e event.Event
+		if err := rows.StructScan(&e); err != nil {
+			log.Errorln("Fail on get event from PG", err)
+		}
+		events = append(events, e)
+	}
+	fmt.Println(events)
 
 	/*
-		opts := []grpc.DialOption{grpc.WithInsecure()}
-
-		conn, err := grpc.Dial(conf.GrpcListen, opts...)
+		rows, err := db.QueryxContext(ctxExec, "select * from events")
 		if err != nil {
-			_, _ = fmt.Fprint(os.Stderr, err)
-			os.Exit(2)
+			log.Errorln("Fail on db quiery:")
+		} else {
+			for rows.Next() {
+				var e event.Event
+				if err := rows.StructScan(&e); err != nil {
+					//TODO
+				}
+				fmt.Println(e)
+			}
 		}
-		client := protobuf.NewCalendarClient(conn)
-		ctx := context.TODO()
+		fmt.Println("************")
+		events := []event.Event{}
+		err = db.Select(&events, "select * from events")
+		fmt.Println(events, err)
+		rows.Close()
+		/*
+			opts := []grpc.DialOption{grpc.WithInsecure()}
 
-		//Event0
-		_, err = client.AddEvent(ctx, &protobuf.Event{StartTime: 1000, EndTime: 2000, Title: "title1", Description: "description1"})
-		fmt.Println(err)
+			conn, err := grpc.Dial(conf.GrpcListen, opts...)
+			if err != nil {
+				_, _ = fmt.Fprint(os.Stderr, err)
+				os.Exit(2)
+			}
+			client := protobuf.NewCalendarClient(conn)
+			ctx := context.TODO()
 
-		//Event1
-		_, err = client.AddEvent(ctx, &protobuf.Event{StartTime: 2000, EndTime: 3000, Title: "title2", Description: "description2"})
-		fmt.Println(err)
+			//Event0
+			_, err = client.AddEvent(ctx, &protobuf.Event{StartTime: 1000, EndTime: 2000, Title: "title1", Description: "description1"})
+			fmt.Println(err)
 
-		//Event2
-		_, err = client.AddEvent(ctx, &protobuf.Event{StartTime: 1000, EndTime: 4000, Title: "title3", Description: "description3"})
-		fmt.Println(err)
+			//Event1
+			_, err = client.AddEvent(ctx, &protobuf.Event{StartTime: 2000, EndTime: 3000, Title: "title2", Description: "description2"})
+			fmt.Println(err)
 
-		grpcEvent, err := client.GetEvent(ctx, &protobuf.Id{Id: 0})
-		fmt.Println(grpcEvent, err)
+			//Event2
+			_, err = client.AddEvent(ctx, &protobuf.Event{StartTime: 1000, EndTime: 4000, Title: "title3", Description: "description3"})
+			fmt.Println(err)
 
-		grpcEvents, err := client.GetAllEvents(ctx, &empty.Empty{})
-		fmt.Println(grpcEvents, err)
+			grpcEvent, err := client.GetEvent(ctx, &protobuf.Id{Id: 0})
+			fmt.Println(grpcEvent, err)
 
-		_, _ = client.DelEvent(ctx, &protobuf.Id{Id: 0})
+			grpcEvents, err := client.GetAllEvents(ctx, &empty.Empty{})
+			fmt.Println(grpcEvents, err)
 
-		grpcEvents2, err := client.GetAllEvents(ctx, &empty.Empty{})
-		fmt.Println(grpcEvents2, err)
+			_, _ = client.DelEvent(ctx, &protobuf.Id{Id: 0})
+
+			grpcEvents2, err := client.GetAllEvents(ctx, &empty.Empty{})
+			fmt.Println(grpcEvents2, err)
 	*/
 }
