11bbc66ba16bb9dd9397e4944f8c9d267cbcbcdd
dark705 <dark705r@mail.ru>
2020-03-01 03:45:28 +0300

    Home work 12 #add isBusy check in PF

--- hw12/cmd/client.go
+++ hw12/cmd/client.go
@@ -39,6 +39,7 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
+	defer db.Close()
 	ctxExec, _ := context.WithTimeout(context.Background(), time.Second*time.Duration(conf.PgTimeoutExecute))
 	/*
 		Add(e event.Event) error
@@ -49,114 +50,117 @@ func main() {
 		IntervalIsBusy(event.Event, bool) (bool, error)
 	*/
 
-	ev, err := event.CreateEvent("2006-01-02T15:00:00Z", "2006-01-02T16:00:00Z", "Event 1", "Some Desc1")
-	if err != nil {
-		log.Errorln("Cant parse", err)
-	}
+	/*
+		//Add
+		sql := "INSERT INTO events (start_time, end_time, title, description) VALUES (:start_time, :end_time, :title, :description);" // :start_time from `db:"start_time"` and so on
+		_, err = db.NamedExecContext(ctxExec, sql, ev)
+		if err != nil {
+			log.Errorln("Fail on add event to PG", err)
+		}
 
-	//Add
-	sql := "INSERT INTO events (start_time, end_time, title, description) VALUES (:start_time, :end_time, :title, :description);" // :start_time from `db:"start_time"` and so on
-	_, err = db.NamedExecContext(ctxExec, sql, ev)
-	if err != nil {
-		log.Errorln("Fail on add event to PG", err)
-	}
-
-	//Get
-	sql = "SELECT * FROM events WHERE id = :id;" // :id from `db:"id"`
-	rows, err := db.NamedQueryContext(ctxExec, sql, struct {
-		Id int `db:"id"`
-	}{Id: 58})
-	if err != nil {
-		log.Errorln("Fail on get event from PG", err)
-	}
-	rows.Next()
-	var e event.Event
-	if err := rows.StructScan(&e); err != nil {
-		log.Errorln("Fail on get event from PG", err)
-	}
-	fmt.Println(e)
-
-	//Del
-	sql = "DELETE FROM events WHERE id = :id;"
-	res, err1 := db.NamedExecContext(ctxExec, sql, struct {
-		Id int `db:"id"`
-	}{Id: 65})
-
-	if err1 != nil {
-		log.Errorln("Fail on del event in PG", err)
-	}
-	aff, err := res.RowsAffected()
-	fmt.Println(aff, err)
-	if aff != 1 || err != nil {
-		log.Errorln("Fail on del event in PG")
-	}
-
-	//GetAll()
-	sql = "SELECT * FROM events;" // :id from `db:"id"`
-	rows, err = db.QueryxContext(ctxExec, sql)
-	if err != nil {
-		log.Errorln("Fail on get event from PG", err)
-	}
-	var events []event.Event
-	for rows.Next() {
+		//Get
+		sql = "SELECT * FROM events WHERE id = :id;" // :id from `db:"id"`
+		rows, err := db.NamedQueryContext(ctxExec, sql, struct {
+			Id int `db:"id"`
+		}{Id: 58})
+		if err != nil {
+			log.Errorln("Fail on get event from PG", err)
+		}
+		rows.Next()
 		var e event.Event
 		if err := rows.StructScan(&e); err != nil {
 			log.Errorln("Fail on get event from PG", err)
 		}
-		events = append(events, e)
+		fmt.Println(e)
+
+		//Del
+		sql = "DELETE FROM events WHERE id = :id;"
+		res, err1 := db.NamedExecContext(ctxExec, sql, struct {
+			Id int `db:"id"`
+		}{Id: 65})
+
+		if err1 != nil {
+			log.Errorln("Fail on del event in PG", err)
+		}
+		aff, err := res.RowsAffected()
+		fmt.Println(aff, err)
+		if aff != 1 || err != nil {
+			log.Errorln("Fail on del event in PG")
+		}
+
+		//GetAll()
+		sql = "SELECT * FROM events;" // :id from `db:"id"`
+		rows, err = db.QueryxContext(ctxExec, sql)
+		if err != nil {
+			log.Errorln("Fail on get event from PG", err)
+		}
+		var events []event.Event
+		for rows.Next() {
+			var e event.Event
+			if err := rows.StructScan(&e); err != nil {
+				log.Errorln("Fail on get event from PG", err)
+			}
+			events = append(events, e)
+		}
+		fmt.Println(events)
+	*/
+
+	//IntervalIsBusy
+	ev, err := event.CreateEvent("2020-02-29T16:10:00Z", "2020-02-29T18:01:00Z", "Event 1", "Some Desc1")
+	if err != nil {
+		log.Errorln("Cant parse", err)
 	}
-	fmt.Println(events)
+	ev.Id = 1
+
+	exist := false
+	var err3 error
+	var rows *sqlx.Rows
+	//SELECT * FROM events WHERE start_time < 'new_end_time' and end_time > 'new_start_time'
+	if exist == true {
+		rows, err3 = db.NamedQueryContext(ctxExec, "SELECT true FROM events WHERE start_time < :end_time AND end_time > :start_time and id != :id;", ev)
+	} else {
+		rows, err3 = db.NamedQueryContext(ctxExec, "SELECT true FROM events WHERE start_time < :end_time AND end_time > :start_time and id != :id;", ev)
+	}
+	if err3 != nil {
+		log.Errorln("Fail on check is busy interval", err)
+	}
+	isBusy := rows.Next()
+	fmt.Println(isBusy)
+
+	//Edit(event.Event) error
 
 	/*
-		rows, err := db.QueryxContext(ctxExec, "select * from events")
+		opts := []grpc.DialOption{grpc.WithInsecure()}
+
+		conn, err := grpc.Dial(conf.GrpcListen, opts...)
 		if err != nil {
-			log.Errorln("Fail on db quiery:")
-		} else {
-			for rows.Next() {
-				var e event.Event
-				if err := rows.StructScan(&e); err != nil {
-					//TODO
-				}
-				fmt.Println(e)
-			}
+			_, _ = fmt.Fprint(os.Stderr, err)
+			os.Exit(2)
 		}
-		fmt.Println("************")
-		events := []event.Event{}
-		err = db.Select(&events, "select * from events")
-		fmt.Println(events, err)
-		rows.Close()
-		/*
-			opts := []grpc.DialOption{grpc.WithInsecure()}
+		client := protobuf.NewCalendarClient(conn)
+		ctx := context.TODO()
 
-			conn, err := grpc.Dial(conf.GrpcListen, opts...)
-			if err != nil {
-				_, _ = fmt.Fprint(os.Stderr, err)
-				os.Exit(2)
-			}
-			client := protobuf.NewCalendarClient(conn)
-			ctx := context.TODO()
+		//Event0
+		_, err = client.AddEvent(ctx, &protobuf.Event{StartTime: 1000, EndTime: 2000, Title: "title1", Description: "description1"})
+		fmt.Println(err)
 
-			//Event0
-			_, err = client.AddEvent(ctx, &protobuf.Event{StartTime: 1000, EndTime: 2000, Title: "title1", Description: "description1"})
-			fmt.Println(err)
+		//Event1
+		_, err = client.AddEvent(ctx, &protobuf.Event{StartTime: 2000, EndTime: 3000, Title: "title2", Description: "description2"})
+		fmt.Println(err)
 
-			//Event1
-			_, err = client.AddEvent(ctx, &protobuf.Event{StartTime: 2000, EndTime: 3000, Title: "title2", Description: "description2"})
-			fmt.Println(err)
+		//Event2
+		_, err = client.AddEvent(ctx, &protobuf.Event{StartTime: 1000, EndTime: 4000, Title: "title3", Description: "description3"})
+		fmt.Println(err)
 
-			//Event2
-			_, err = client.AddEvent(ctx, &protobuf.Event{StartTime: 1000, EndTime: 4000, Title: "title3", Description: "description3"})
-			fmt.Println(err)
+		grpcEvent, err := client.GetEvent(ctx, &protobuf.Id{Id: 0})
+		fmt.Println(grpcEvent, err)
 
-			grpcEvent, err := client.GetEvent(ctx, &protobuf.Id{Id: 0})
-			fmt.Println(grpcEvent, err)
+		grpcEvents, err := client.GetAllEvents(ctx, &empty.Empty{})
+		fmt.Println(grpcEvents, err)
 
-			grpcEvents, err := client.GetAllEvents(ctx, &empty.Empty{})
-			fmt.Println(grpcEvents, err)
+		_, _ = client.DelEvent(ctx, &protobuf.Id{Id: 0})
 
-			_, _ = client.DelEvent(ctx, &protobuf.Id{Id: 0})
-
-			grpcEvents2, err := client.GetAllEvents(ctx, &empty.Empty{})
-			fmt.Println(grpcEvents2, err)
+		grpcEvents2, err := client.GetAllEvents(ctx, &empty.Empty{})
+		fmt.Println(grpcEvents2, err)
 	*/
 }
