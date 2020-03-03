a0d44ca4bd2c47e008fe8d5a54c324761f0a58a1
Sergey Gulinov <sgulinov@stream.ru>
2020-03-03 15:56:35 +0300

    Home work 12 #fix codes error from hw11

--- hw12/internal/grpc/server.go
+++ hw12/internal/grpc/server.go
@@ -12,6 +12,8 @@ import (
 	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type Server struct {
@@ -50,19 +52,31 @@ type CalendarServerGrpc struct {
 
 func (s *Server) AddEvent(ctx context.Context, grpcE *protobuf.Event) (*empty.Empty, error) {
 	s.Logger.Debug("Income gRPC AddEvent() event: ", grpcE)
+
 	err := s.Calendar.AddEvent(event.Event{
 		StartTime:   time.Unix(grpcE.StartTime, 0),
 		EndTime:     time.Unix(grpcE.EndTime, 0),
 		Title:       grpcE.Title,
 		Description: grpcE.Description,
 	})
+	if err != nil {
+		s.Logger.Errorln("Error on Calendar, add:", err)
+		if err == calendar.ErrDateBusy {
+			return &empty.Empty{}, status.Error(codes.AlreadyExists, "Date interval for new event already busy")
+		}
+		return &empty.Empty{}, status.Error(codes.Internal, "Internal server error")
+	}
 
-	return &empty.Empty{}, err
+	return &empty.Empty{}, nil
 }
 
 func (s *Server) GetEvent(ctx context.Context, grpcId *protobuf.Id) (*protobuf.Event, error) {
 	s.Logger.Debug("Income gRPC GetEvent() id:", grpcId)
 	calendarEvent, err := s.Calendar.GetEvent(int(grpcId.Id))
+	if err != nil {
+		s.Logger.Errorln("Error on Calendar, get:", err)
+		return &protobuf.Event{}, status.Error(codes.NotFound, "Even not found")
+	}
 
 	return &protobuf.Event{
 		Id:          int32(calendarEvent.Id),
@@ -70,34 +84,48 @@ func (s *Server) GetEvent(ctx context.Context, grpcId *protobuf.Id) (*protobuf.E
 		EndTime:     calendarEvent.EndTime.Unix(),
 		Title:       calendarEvent.Title,
 		Description: calendarEvent.Description,
-	}, err
+	}, nil
 }
 
 func (s *Server) DelEvent(ctx context.Context, grpcId *protobuf.Id) (*empty.Empty, error) {
 	s.Logger.Debug("Income gRPC DelEvent() id:", grpcId)
+	err := s.Calendar.DelEvent(int(grpcId.Id))
+	if err != nil {
+		s.Logger.Errorln("Error on Calendar, delete:", err)
+		return &empty.Empty{}, status.Error(codes.NotFound, "Even not found")
+	}
 
-	return &empty.Empty{}, s.Calendar.DelEvent(int(grpcId.Id))
+	return &empty.Empty{}, nil
 }
 
 func (s *Server) EditEvent(ctx context.Context, grpcE *protobuf.Event) (*empty.Empty, error) {
 	s.Logger.Debug("Income gRPC EditEvent() event:", grpcE)
-
 	err := s.Calendar.EditEvent(event.Event{
 		StartTime:   time.Unix(grpcE.StartTime, 0),
 		EndTime:     time.Unix(grpcE.EndTime, 0),
 		Title:       grpcE.Title,
 		Description: grpcE.Description,
 	})
+	if err != nil {
+		s.Logger.Errorln("Error on Calendar, edit:", err)
+		if err == calendar.ErrDateBusy {
+			return &empty.Empty{}, status.Error(codes.AlreadyExists, "Date interval for new event already busy")
+		}
+		return &empty.Empty{}, status.Error(codes.Internal, "Internal server error")
+	}
 
-	return &empty.Empty{}, err
+	return &empty.Empty{}, nil
 }
 
 func (s *Server) GetAllEvents(ctx context.Context, ev *empty.Empty) (*protobuf.Events, error) {
 	s.Logger.Debug("Income gRPC GetAllEvents()")
 	calendarEvents, err := s.Calendar.GetAllEvents()
 	l := len(calendarEvents)
-
 	protobufEvents := make([]*protobuf.Event, 0, l)
+	if err != nil {
+		s.Logger.Errorln("Error to get all Events calendar", err)
+		return &protobuf.Events{Events: protobufEvents}, status.Error(codes.Internal, "Internal server error")
+	}
 
 	for _, calendarEvent := range calendarEvents {
 		protobufEvent := protobuf.Event{
@@ -110,5 +138,5 @@ func (s *Server) GetAllEvents(ctx context.Context, ev *empty.Empty) (*protobuf.E
 		protobufEvents = append(protobufEvents, &protobufEvent)
 	}
 
-	return &protobuf.Events{Events: protobufEvents}, err
+	return &protobuf.Events{Events: protobufEvents}, nil
 }
