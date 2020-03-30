1a94c55e1b50661f08cc74786d207f570b4033e4
Sergey Gulinov <sgulinov@stream.ru>
2020-03-31 00:25:39 +0300

    Home work 16# improve grpc test

--- hw16/tests/features/grpc.feature
+++ hw16/tests/features/grpc.feature
@@ -3,8 +3,8 @@
 
 Feature: gRPC api
 
-  Scenario: Add event in empty DB
-    When I send execute AddEvent on "localhost:8889" Event data:
+  Scenario: Execute AddEvent and then GetEvent in empty DB, return same event, after DelEvent
+    When I send execute AddEvent on "localhost:8889" with data:
 		"""
 		{
 			"Id": 1,
@@ -15,4 +15,17 @@ Feature: gRPC api
 		}
 		"""
     Then The response error code should be nil
-    And The execute GetAllEvents return one same Event
+    And The execute GetEvent return same Event, with no error code
+    And After Execute DelEvent, return no error code
+
+  Scenario: Execute AddEvent two times on same event return error of busy day, and in Calendar only one event
+    When I send execute AddEvent on "localhost:8889" two times with data:
+		"""
+		{
+			"StartTime": 1000,
+			"EndTime": 2000,
+			"Title": "title1",
+			"Description": "description1"
+		}
+		"""
+    Then The response error desc should be "Date interval for new event already busy"
--- hw16/tests/grpc_test.go
+++ hw16/tests/grpc_test.go
@@ -12,22 +12,22 @@ import (
 	"github.com/cucumber/godog/gherkin"
 	"github.com/dark705/otus/hw16/pkg/calendar/protobuf"
 	"github.com/golang/protobuf/proto"
-	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
 )
 
-var ctx context.Context
 var grpcError error
 var client protobuf.CalendarClient
 var sendGrpcEvent protobuf.Event
 
-func iSendExecuteAddEventOnEventData(addr string, data *gherkin.DocString) error {
-	ctxConn, _ := context.WithTimeout(context.Background(), time.Second*2)
+func iSendExecuteAddEventOnWithData(addr string, data *gherkin.DocString) error {
+	ctxConn, _ := context.WithTimeout(context.Background(), time.Second*10)
 	conn, err := grpc.DialContext(ctxConn, addr, []grpc.DialOption{grpc.WithInsecure(), grpc.WithBlock()}...)
 	if err != nil {
 		err = fmt.Errorf("can't connect to grpc server, error: %v", err)
 		return err
 	}
+	client = protobuf.NewCalendarClient(conn)
 
 	replacer := strings.NewReplacer("\n", "", "\t", "")
 	cleanJson := replacer.Replace(data.Content)
@@ -37,12 +37,9 @@ func iSendExecuteAddEventOnEventData(addr string, data *gherkin.DocString) error
 		err = fmt.Errorf("can't unmarshal, error: %v", err)
 		return err
 	}
-	fmt.Println(cleanJson)
 
-	client = protobuf.NewCalendarClient(conn)
-	ctx = context.TODO()
-	_, grpcError = client.AddEvent(ctx, &sendGrpcEvent)
-	return grpcError
+	_, grpcError = client.AddEvent(context.TODO(), &sendGrpcEvent)
+	return nil
 }
 
 func theResponseErrorCodeShouldBeNil() (err error) {
@@ -52,26 +49,68 @@ func theResponseErrorCodeShouldBeNil() (err error) {
 	return err
 }
 
-func theExecuteGetAllEventsReturnOneSameEvent() error {
-	grpcEvents, err := client.GetAllEvents(ctx, &empty.Empty{})
+func theExecuteGetEventReturnSameEventWithNoErrorCode() error {
+	getGrpcEvent, err := client.GetEvent(context.TODO(), &protobuf.Id{Id: 1})
 	if err != nil {
 		return err
 	}
-	if len(grpcEvents.Events) != 1 {
-		return errors.New("grpc client returned not one Event")
-	}
 
-	if !proto.Equal(&sendGrpcEvent, grpcEvents.Events[0]) {
-		fmt.Println(&sendGrpcEvent)
-		fmt.Println(grpcEvents.Events[0])
+	if !proto.Equal(&sendGrpcEvent, getGrpcEvent) {
 		return errors.New("add and Get Event's not same")
 	}
 	return nil
 }
 
-func FeatureGrpcContext(s *godog.Suite) {
-	s.Step(`^I send execute AddEvent on "([^"]*)" Event data:$`, iSendExecuteAddEventOnEventData)
-	s.Step(`^The response error code should be nil$`, theResponseErrorCodeShouldBeNil)
-	s.Step(`^The execute GetAllEvents return one same Event$`, theExecuteGetAllEventsReturnOneSameEvent)
+func afterExecuteDelEventReturnNoErrorCode() error {
+	_, err := client.DelEvent(context.TODO(), &protobuf.Id{Id: 1})
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
+func iSendExecuteAddEventOnTwoTimesWithData(addr string, data *gherkin.DocString) error {
+	ctxConn, _ := context.WithTimeout(context.Background(), time.Second*10)
+	conn, err := grpc.DialContext(ctxConn, addr, []grpc.DialOption{grpc.WithInsecure(), grpc.WithBlock()}...)
+	if err != nil {
+		err = fmt.Errorf("can't connect to grpc server, error: %v", err)
+		return err
+	}
+	client = protobuf.NewCalendarClient(conn)
+
+	replacer := strings.NewReplacer("\n", "", "\t", "")
+	cleanJson := replacer.Replace(data.Content)
+
+	err = json.Unmarshal([]byte(cleanJson), &sendGrpcEvent)
+	if err != nil {
+		err = fmt.Errorf("can't unmarshal, error: %v", err)
+		return err
+	}
+
+	_, err = client.AddEvent(context.TODO(), &sendGrpcEvent)
+	if err != nil {
+		return err
+	}
+	_, grpcError = client.AddEvent(context.TODO(), &sendGrpcEvent)
+	return nil
+}
+
+func theResponseErrorDescShouldBe(desc string) error {
+	s, ok := status.FromError(grpcError)
+	if !ok {
+		return errors.New("can't get status from grpcError")
+	}
+	if desc != s.Message() {
+		return errors.New("wrong desc on error busy")
+	}
+	return nil
+}
+
+func FeatureGrpcContext(s *godog.Suite) {
+	s.Step(`^I send execute AddEvent on "([^"]*)" with data:$`, iSendExecuteAddEventOnWithData)
+	s.Step(`^The response error code should be nil$`, theResponseErrorCodeShouldBeNil)
+	s.Step(`^The execute GetEvent return same Event, with no error code$`, theExecuteGetEventReturnSameEventWithNoErrorCode)
+	s.Step(`^After Execute DelEvent, return no error code$`, afterExecuteDelEventReturnNoErrorCode)
+	s.Step(`^I send execute AddEvent on "([^"]*)" two times with data:$`, iSendExecuteAddEventOnTwoTimesWithData)
+	s.Step(`^The response error desc should be "([^"]*)"$`, theResponseErrorDescShouldBe)
 }
