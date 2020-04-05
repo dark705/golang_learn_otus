473c86621668a1f8772d6acca849c5efb80eea5b
Sergey Gulinov <sgulinov@stream.ru>
2020-04-05 18:52:24 +0300

    Home work 16# improve from hw15

--- hw16/build/docker.build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker build --tag hw16_postgres:latest -f ./build/docker/postgres/Dockerfile ./
-docker build --tag hw16_rabbitmq:latest -f ./build/docker/rabbitmq/Dockerfile ./
-
-docker build --tag hw16_calendar_scheduler:latest -f ./build/docker/calendar/scheduler/Dockerfile ./
-docker build --tag hw16_calendar_sender:latest -f ./build/docker/calendar/sender/Dockerfile ./
-docker build --tag hw16_calendar_api:latest -f ./build/docker/calendar/api/Dockerfile ./
-docker build --tag hw16_calendar_godog:latest -f ./build/docker/godog/Dockerfile ./
--- hw16/build/docker.clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker image rm hw16_postgres:latest --force
-docker image rm hw16_rabbitmq:latest --force
-
-docker image rm hw16_calendar_scheduler:latest --force
-docker image rm hw16_calendar_sender:latest --force
-docker image rm hw16_calendar_api:latest --force
-docker image rm hw16_calendar_godog:latest --force
